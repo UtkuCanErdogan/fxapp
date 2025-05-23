9babc90fefa27f7cc65391566baa45c9854f67c6
Utku Can Erdogan <utku.e.dgn@gmail.com>
2025-05-24 00:36:52 +0300

    chore: initial commit

---
+++ src/test/java/com/utkucan/fxapp/FxappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkucan.fxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
