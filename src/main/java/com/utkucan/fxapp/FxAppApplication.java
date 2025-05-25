95c6147a517afb7ffaf154f7daf72136368d8cc0
Utku Can Erdogan <utku.e.dgn@gmail.com>
2025-05-25 22:43:31 +0300

    feat: exchange rate sync service added and scheduled

--- src/main/java/com/utkucan/fxapp/FxAppApplication.java
+++ src/main/java/com/utkucan/fxapp/FxAppApplication.java
@@ -2,8 +2,10 @@ package com.utkucan.fxapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FxAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/utkucan/fxapp/application/service/ExchangeRateSyncService.java
@@ -0,0 +1,46 @@
+package com.utkucan.fxapp.application.service;
+
+import com.utkucan.fxapp.domain.enums.CurrencyCode;
+import com.utkucan.fxapp.domain.entity.Currency;
+import com.utkucan.fxapp.domain.repository.CurrencyRateProvider;
+import com.utkucan.fxapp.domain.repository.CurrencyRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class ExchangeRateSyncService {
+
+    private final CurrencyRateProvider currencyRateProvider;
+    private final CurrencyRepository currencyRepository;
+
+    public ExchangeRateSyncService(CurrencyRateProvider currencyRateProvider, CurrencyRepository currencyRepository) {
+        this.currencyRateProvider = currencyRateProvider;
+        this.currencyRepository = currencyRepository;
+    }
+
+
+    @Scheduled(fixedRateString = "${openExchangeRate.interval}")
+    public void syncLatestRates() {
+        CurrencyCode currencyCode = CurrencyCode.USD;
+        Map<String, Double> rates = currencyRateProvider.getCurrentRates(currencyCode);
+
+        long timestamp = System.currentTimeMillis();
+
+        if (rates != null) {
+            List<CurrencyCode> currencyCodes = CurrencyCode.getDataSet();
+            Set<Currency> currencySet = currencyCodes.stream().map(c -> {
+                Double rate = rates.get(c.toString());
+
+                return new Currency(c.toString(), rate, currencyCode, timestamp);
+            }).collect(Collectors.toSet());
+
+            currencyRepository.saveAll(currencySet);
+        } else {
+            //ToDo: throw exception
+        }
+    }
+}
