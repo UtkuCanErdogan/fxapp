bec9e771f8ba9cfcc547e761073a15d56ff09318
Utku Can Erdogan <utku.e.dgn@gmail.com>
2025-05-26 21:41:20 +0300

    chore: open api implementation added

--- src/main/java/com/utkucan/fxapp/FxAppApplication.java
+++ src/main/java/com/utkucan/fxapp/FxAppApplication.java
@@ -1,9 +1,18 @@
 package com.utkucan.fxapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Foreign Exchange API",
+				version = "1.0",
+				description = "Provides currency conversion and exchange rate services"
+		)
+)
 @SpringBootApplication
 @EnableScheduling
 public class FxAppApplication {
