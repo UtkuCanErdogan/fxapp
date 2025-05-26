01843641576c2d6dd1212d3bf76db8bf969a8473
Utku Can Erdogan <utku.e.dgn@gmail.com>
2025-05-26 21:39:15 +0300

    refactor: currency rate type changed from double to big decimal

--- src/main/java/com/utkucan/fxapp/domain/entity/Currency.java
+++ src/main/java/com/utkucan/fxapp/domain/entity/Currency.java
@@ -3,6 +3,7 @@ package com.utkucan.fxapp.domain.entity;
 import com.utkucan.fxapp.domain.enums.CurrencyCode;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
@@ -12,10 +13,11 @@ import java.math.BigDecimal;
 @RedisHash("Currency")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class Currency implements Serializable {
     @Id
     private String id;
-    private Double rate;
+    private BigDecimal rate;
     private CurrencyCode base;
     private Long timestamp;
 }
