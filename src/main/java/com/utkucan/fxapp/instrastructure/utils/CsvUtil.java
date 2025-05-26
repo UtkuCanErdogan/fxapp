4831a4d5767bbe55ac05d7819759831829dea45f
Utku Can Erdogan <utku.e.dgn@gmail.com>
2025-05-26 21:52:35 +0300

    feat: implement CSV parsing utility for bulk import functionality

---
+++ src/main/java/com/utkucan/fxapp/instrastructure/utils/CsvUtil.java
@@ -0,0 +1,47 @@
+package com.utkucan.fxapp.instrastructure.utils;
+
+import com.utkucan.fxapp.application.dto.request.ExchangeRequest;
+import com.utkucan.fxapp.domain.enums.CurrencyCode;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvUtil {
+
+    public static List<ExchangeRequest> parseCsvFileToExchangeRequest(MultipartFile file) throws IOException {
+        List<ExchangeRequest> requests = new ArrayList<>();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+        String line;
+        boolean skipHeader = true;
+
+        while ((line = reader.readLine()) != null) {
+            if (skipHeader) {
+                skipHeader = false;
+                continue;
+            }
+
+            String[] parts = line.split(",");
+            if (parts.length != 3) continue;
+
+            try {
+                String from = parts[0].trim();
+                String to = parts[1].trim();
+                BigDecimal amount = new BigDecimal(parts[2].trim());
+
+                CurrencyCode fromCurrency = CurrencyCode.valueOf(from);
+                CurrencyCode toCurrency = CurrencyCode.valueOf(to);
+                requests.add(new ExchangeRequest(fromCurrency, toCurrency, amount));
+            } catch (Exception e) {
+                // log & skip
+            }
+        }
+
+        return requests;
+    }
+}
