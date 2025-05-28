fd034da45444d8ce3888820f903e9044dbb59b3f
Utku Can Erdogan <utku.e.dgn@gmail.com>
2025-05-28 11:30:37 +0300

    feat: invalid file test added

--- src/test/java/com/utkucan/fxapp/application/service/ExchangeServiceTest.java
+++ src/test/java/com/utkucan/fxapp/application/service/ExchangeServiceTest.java
@@ -1,10 +1,12 @@
 package com.utkucan.fxapp.application.service;
 
+import com.utkucan.fxapp.application.dto.request.CsvUploadRequest;
 import com.utkucan.fxapp.application.dto.request.ExchangeRequest;
 import com.utkucan.fxapp.application.dto.response.BulkCsvResponse;
 import com.utkucan.fxapp.application.dto.response.ExchangeCsvResponse;
 import com.utkucan.fxapp.application.dto.response.ExchangeResponse;
-import com.utkucan.fxapp.application.service.ExchangeService;
+import com.utkucan.fxapp.common.exception.ExceptionCode;
+import com.utkucan.fxapp.common.exception.FileReadException;
 import com.utkucan.fxapp.domain.entity.Currency;
 import com.utkucan.fxapp.domain.enums.CurrencyCode;
 import com.utkucan.fxapp.domain.repository.CurrencyRepository;
@@ -137,8 +139,8 @@ class ExchangeServiceTest {
     @Test
     void shouldProcessAllExchangesSuccessfully_WhenValidCsvFilesProvided() throws Exception {
         Map<String, List<String[]>> fileInputMap = new HashMap<>();
-        ClassPathResource valid1 = new ClassPathResource("test-csv/success_bulk1.csv");
-        ClassPathResource valid2 = new ClassPathResource("test-csv/success_bulk2.csv");
+        ClassPathResource valid1 = new ClassPathResource("test-files/success_bulk1.csv");
+        ClassPathResource valid2 = new ClassPathResource("test-files/success_bulk2.csv");
 
         MockMultipartFile file1 = new MockMultipartFile("files", "success_bulk1.csv", "text/csv", valid1.getInputStream());
         MockMultipartFile file2 = new MockMultipartFile("files", "success_bulk2.csv", "text/csv", valid2.getInputStream());
@@ -156,7 +158,7 @@ class ExchangeServiceTest {
             fileInputMap.put(file.getOriginalFilename(), csvLines);
         }
 
-        List<BulkCsvResponse> responses = exchangeService.processBulkCsv(new MultipartFile[]{file1, file2});
+        List<BulkCsvResponse> responses = exchangeService.processBulkCsv(new CsvUploadRequest(new MultipartFile[]{file1, file2}));
 
         assertThat(responses).hasSize(2);
 
@@ -188,15 +190,35 @@ class ExchangeServiceTest {
 
     @Test
     void shouldFail_WhenCsvFileContainsSameCurrencyPairs() throws Exception {
-        ClassPathResource invalidFile = new ClassPathResource("test-csv/failed_bulk.csv");
+        ClassPathResource invalidFile = new ClassPathResource("test-files/failed_bulk.csv");
         MockMultipartFile file = new MockMultipartFile("files", "failed_bulk.csv", "text/csv", invalidFile.getInputStream());
 
         ConstraintViolationException ex = catchThrowableOfType(() ->
-                        exchangeService.processBulkCsv(new MultipartFile[]{file}),
+                        exchangeService.processBulkCsv(new CsvUploadRequest(new MultipartFile[]{file})),
                 ConstraintViolationException.class
         );
 
         assertThat(ex.getConstraintViolations())
                 .anySatisfy(v -> assertThat(v.getMessage()).contains("Source and target currencies must be different"));
+    }
+
+    @Test
+    void shouldThrowException_WhenNonCsvFileUploaded() throws Exception {
+        ClassPathResource invalidTxt = new ClassPathResource("test-files/invalid_file.txt");
+        MockMultipartFile file = new MockMultipartFile(
+                "files",
+                "fake_broken.txt",
+                "text/plain",
+                invalidTxt.getInputStream()
+        );
+
+        FileReadException ex = catchThrowableOfType(() ->
+                        exchangeService.processBulkCsv(new CsvUploadRequest(new MultipartFile[]{file})),
+                FileReadException.class
+        );
+
+        assertThat(ex)
+                .isInstanceOf(FileReadException.class)
+                .hasMessageContaining(ExceptionCode.FILE_READ_EXCEPTION.message());
     }
 }
